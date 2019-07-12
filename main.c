2233ac5fd8f97e32417766203a9486a8e10d5c56
jbooth <joshua.booth@nrl.navy.mil>
2019-07-12 16:41:51 -0400

    started

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+void draw(int n){
+	// Find needed space
+	int digits_of_n = floor(log10(n)) + 1;
+	printf("%d\n", digits_of_n);
+	for(int current_row = 1; current_row < n; current_row++){
+		
+	}
+}
+int main(void){
+	draw(5);	
+}
